5ce8b1738106113646d1c2e8205568f45755d644
Pedro Ramos <pedroramosromero@gmail.com>
2024-12-09 19:04:04 +0100

    Integrate applitools into the project

---
+++ applitools.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+
+    // Uncomment the below line and paste in your API key as an alternative to using an environment variable
+    apiKey: '',
+
+    batchName: 'NGX Demo Visual Testing',
+
+    // Select the browsers and devices to run your tests on via the Ultrafast Grid
+    // browser: [
+    //     {width: 800, height: 600, name: 'chrome'},
+    //     {width: 1600, height: 1200, name: 'firefox'},
+    //     {width: 1024, height: 768, name: 'safari'},
+    //     {deviceName: 'Pixel 2', screenOrientation: 'portrait'},
+    //     {deviceName: 'Nexus 10', screenOrientation: 'landscape'},
+    // ]
+}
