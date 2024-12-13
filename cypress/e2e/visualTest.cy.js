6eec3484e7273ec3a6f3534495a88be95132948a
Pedro Ramos <pedroramosromero@gmail.com>
2024-12-13 20:45:44 +0100

    Include 'eyes' methods in the test

--- cypress/e2e/visualTest.cy.js
+++ cypress/e2e/visualTest.cy.js
@@ -1,11 +1,28 @@
 describe('visual testing', () => {
+    beforeEach(() => {
+        // Start Applitools Visual AI Test
+        cy.eyesOpen({
+            appName: 'NGX Cypress Test App',
+            testName: Cypress.currentTest.title
+        })
+    })
 
-    it('test with applitools 5', () => {
-        cy.visit('http://localhost:4200/')
+    afterEach(() => {
+        // End Applitools Visual AI Test
+        cy.eyesClose()
+    })
+
+    it('test with applitools demo app', () => {
+        cy.visit(' ')
+        cy.eyesCheckWindow({
+            tag: "Home page",
+        })
 
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
-        
+        cy.eyesCheckWindow({
+            tag: "Form Layouts",
+        })
     }) 
 
 })
