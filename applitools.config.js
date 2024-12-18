9da8d6dc550b18c04e48e0a1c2cee2007a3df698
Pedro Ramos <pedroramosromero@gmail.com>
2024-12-18 23:16:26 +0100

    Include different browsers, screeens and devices for visual testing

--- applitools.config.js
+++ applitools.config.js
@@ -7,11 +7,11 @@ module.exports = {
     batchName: 'Playground Demo Visual Testing',
 
     // Select the browsers and devices to run your tests on via the Ultrafast Grid
-    // browser: [
-    //     {width: 800, height: 600, name: 'chrome'},
-    //     {width: 1600, height: 1200, name: 'firefox'},
-    //     {width: 1024, height: 768, name: 'safari'},
-    //     {deviceName: 'Pixel 2', screenOrientation: 'portrait'},
-    //     {deviceName: 'Nexus 10', screenOrientation: 'landscape'},
-    // ]
+    browser: [
+        {width: 800, height: 600, name: 'chrome'},
+        {width: 1600, height: 1200, name: 'firefox'},
+        {width: 1024, height: 768, name: 'safari'},
+        {deviceName: 'Pixel 2', screenOrientation: 'portrait'},
+        {deviceName: 'Nexus 10', screenOrientation: 'landscape'},
+    ]
 }
